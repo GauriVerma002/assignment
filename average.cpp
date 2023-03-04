dbc6c26cda77b8f1a9203e83235a1d8e3b249bac
Gauri Verma <114560615+GauriVerma002@users.noreply.github.com>
2023-03-04 16:00:57 +0800

    Create average.cpp

---
+++ average.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int avg, a, b, c;
+    cout << "Enter three values to calculate average  ";
+    cin >> a >> b >> c;
+    avg = (a + b + c)/3;
+    cout << "Average =" << avg << endl;
+    return 0;
+}

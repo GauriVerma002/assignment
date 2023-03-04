add63c03f853153ec34c846336862ef4595d88af
Gauri Verma <114560615+GauriVerma002@users.noreply.github.com>
2023-03-04 16:01:44 +0800

    Create square.cpp

---
+++ square.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int val,sqr;
+    cout << "Enter a value to calculate its square ";
+    cin >> val;
+    sqr= val* val;
+    cout << "Square of this number =" << sqr << endl;
+    return 0;
+}

213f4108c5d9bac8286c2ec3c2a22f1a02744f1d
Gauri Verma <114560615+GauriVerma002@users.noreply.github.com>
2023-03-04 16:02:45 +0800

    Create max.cpp

---
+++ max.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, max;
+    cout << "Enter two number to find maximum ";
+    cin >> a >> b;
+    if(a>b)
+    cout << "maximum is= " << a <<endl;
+    else
+    cout << "maximum is= "<< b <<endl;
+    return 0;
+}

d7d7849f226e44a90148f0f728abe34372b0c536
Gauri Verma <114560615+GauriVerma002@users.noreply.github.com>
2023-03-04 15:59:22 +0800

    Create areaofcircle.cpp

---
+++ areaofcircle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int area, r, pie = 3.14;
+    cin >> r;
+    area = pie * r * r;
+    cout << "Area of Circle =" << area << endl;
+    return 0;
+}
